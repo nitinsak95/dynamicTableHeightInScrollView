5d960d33c82df65d0d382ac509bd3fd287a3735a
affixusimac1 <viveking@outlook.com>
2018-10-17 13:13:47 +0530

    Commit

---
+++ tabheight/Cell1.swift
@@ -0,0 +1,26 @@
+//
+//  Cell1.swift
+//  tabheight
+//
+//  Created by AFFIXUS IMAC1 on 9/6/18.
+//  Copyright © 2018 AFFIXUS IMAC1. All rights reserved.
+//
+
+import UIKit
+
+class Cell1: UITableViewCell {
+
+    @IBOutlet weak var lbl1: UILabel!
+    @IBOutlet weak var lbl2: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- tabheight/ViewController.swift
+++ tabheight/ViewController.swift
@@ -8,16 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tblHeighConst: NSLayoutConstraint!
+    @IBOutlet weak var vwConstant: NSLayoutConstraint!
+    
+    
+    var names = ["Neymar", "Messi", "Salah", "Lukaku", "Modric", "Cavani", "Benzema", "Pogba", "Benzema", "Pogba", "Messi", "Salah", "Lukaku", "Modric", "Cavani", "Benzema", "Pogba", "Benzema", "Pogba"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.reloadData()
+        
+        tblHeighConst.constant =  CGFloat(names.count * 80)
+        vwConstant.constant = CGFloat(names.count * 80) + 100
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! Cell1
+        cell.lbl1.text = names[indexPath.row]
+        cell.lbl2.text = names[indexPath.row]
+        return cell
     }
 
 
